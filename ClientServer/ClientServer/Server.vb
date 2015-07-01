40850e592720cf49d605a60a1076bd06d7abbe5a
Michael Skoczen <mike@pontech.com>
2015-07-01 09:30:07 -0700

    Made server work as threads

--- ClientServer/ClientServer/Server.vb
+++ ClientServer/ClientServer/Server.vb
@@ -1,131 +1,109 @@
 ﻿Imports System.Net.Sockets
+Imports System.Net
 Imports System.Text
+Imports System.Threading
 
 Public Class Server
-    'Module Module1
-    '        Dim clientsList As New Hashtable
-    '        Sub Main()
-    '            Dim serverSocket As New TcpListener(8888)
-    '            Dim clientSocket As TcpClient
-    '            Dim infiniteCounter As Integer
-    '            Dim counter As Integer
+    Public serverSocket As TcpListener
+    Dim clientsList As New Hashtable
+    Public Sub New(ByVal Address As IPAddress, ByVal Port As Integer)
+        'System.Net.NetworkInformation.IPAddressInformation
+        serverSocket = New TcpListener(Address, Port)
+        'serverSocket = New TcpListener(Port)
+        serverSocket.Start()
+        'msg("Chat Server Started ....")
+        Dim HandleNewClientsThread = New Thread(AddressOf HandleNewClients)
+        HandleNewClientsThread.IsBackground = True
+        HandleNewClientsThread.Name = "HandleNewClientsThread"
+        HandleNewClientsThread.Start()
+    End Sub
 
-    '            serverSocket.Start()
-    '            msg("Chat Server Started ....")
-    '            counter = 0
-    '            infiniteCounter = 0
-    '            For infiniteCounter = 1 To 2
-    '                infiniteCounter = 1
-    '                counter += 1
-    '                clientSocket = serverSocket.AcceptTcpClient()
+    Public Sub ExitServer()
+        'msg("exit")
+        'clientSocket.Close()
+        'serverSocket.Stop()
+    End Sub
 
-    '                Dim bytesFrom(10024) As Byte
-    '                Dim dataFromClient As String
+    Private Sub HandleNewClients()
+        While True
+            Dim clientSocket As TcpClient
+            clientSocket = serverSocket.AcceptTcpClient()
+            Dim bytesFrom(10024) As Byte
+            Dim dataFromClient As String
 
-    '                Dim networkStream As NetworkStream = _
-    '                clientSocket.GetStream()
-    '                networkStream.Read(bytesFrom, 0, CInt(clientSocket.ReceiveBufferSize))
-    '                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom)
-    '                dataFromClient = _
-    '                dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
+            Dim networkStream As NetworkStream = clientSocket.GetStream()
+            networkStream.Read(bytesFrom, 0, CInt(clientSocket.ReceiveBufferSize))
+            dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom)
+            dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
 
-    '                clientsList(dataFromClient) = clientSocket
+            clientsList(dataFromClient) = clientSocket
 
-    '                broadcast(dataFromClient + " Joined ", dataFromClient, False)
+            broadcast(dataFromClient + " Joined ", dataFromClient, False)
 
-    '                msg(dataFromClient + " Joined chat room ")
-    '                Dim client As New handleClinet
-    '                client.startClient(clientSocket, dataFromClient, clientsList)
-    '            Next
+            'msg(dataFromClient + " Joined chat room ")
+            Dim HandleClientThread = New Thread(AddressOf handleClientNew)
+            HandleClientThread.IsBackground = True
+            HandleClientThread.Name = "HandleClientThread"
+            Dim parameters As New handleClientData()
+            parameters.clientSocket = clientSocket
+            parameters.clName = dataFromClient
+            HandleClientThread.Start(parameters)
+        End While
+    End Sub
+    Private Sub broadcast(ByVal msg As String, ByVal uName As String, ByVal flag As Boolean)
+        Dim Item As DictionaryEntry
+        For Each Item In clientsList
+            Dim broadcastSocket As TcpClient
+            broadcastSocket = CType(Item.Value, TcpClient)
+            Dim broadcastStream As NetworkStream = broadcastSocket.GetStream()
+            Dim broadcastBytes As [Byte]()
 
-    '            clientSocket.Close()
-    '            serverSocket.Stop()
-    '            msg("exit")
-    '            Console.ReadLine()
-    '        End Sub
+            If flag = True Then
+                broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + msg)
+            Else
+                broadcastBytes = Encoding.ASCII.GetBytes(msg)
+            End If
 
-    '        Sub msg(ByVal mesg As String)
-    '            mesg.Trim()
-    '            Console.WriteLine(" >> " + mesg)
-    '        End Sub
-    '        Private Sub broadcast(ByVal msg As String, _
-    '        ByVal uName As String, ByVal flag As Boolean)
-    '            Dim Item As DictionaryEntry
-    '            For Each Item In clientsList
-    '                Dim broadcastSocket As TcpClient
-    '                broadcastSocket = CType(Item.Value, TcpClient)
-    '                Dim broadcastStream As NetworkStream = _
-    '                        broadcastSocket.GetStream()
-    '                Dim broadcastBytes As [Byte]()
+            broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length)
+            broadcastStream.Flush()
+        Next
+    End Sub
 
-    '                If flag = True Then
-    '                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + msg)
-    '                Else
-    '                    broadcastBytes = Encoding.ASCII.GetBytes(msg)
-    '                End If
+    Public Class handleClientData
+        Public clientSocket As TcpClient
+        Public clName As String
+    End Class
 
-    '                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length)
-    '                broadcastStream.Flush()
-    '            Next
-    '        End Sub
+    Private Function ParceMessage(ByVal message As String) As String
+        Dim response As String = message
+        If message.ToLower = "time" Then
+            response = Now.ToShortDateString
+        End If
+        Return response
+    End Function
 
-    '        Public Class handleClinet
-    '            Dim clientSocket As TcpClient
-    '            Dim clNo As String
-    '            Dim clientsList As Hashtable
+    Public Sub handleClientNew(ByVal clientdata As Object)
+        Dim clientSocket As TcpClient = CType(clientdata, handleClientData).clientSocket
+        Dim clName As String = CType(clientdata, handleClientData).clName
 
-    '            Public Sub startClient(ByVal inClientSocket As TcpClient, _
-    '            ByVal clineNo As String, ByVal cList As Hashtable)
-    '                Me.clientSocket = inClientSocket
-    '                Me.clNo = clineNo
-    '                Me.clientsList = cList
-    '                Dim ctThread As Threading.Thread = New Threading.Thread(AddressOf doChat)
-    '                ctThread.Start()
-    '            End Sub
+        Dim bytesFrom(10024) As Byte
+        Dim dataFromClient As String
+        While True
+            Try
+                Dim networkStream As NetworkStream = clientSocket.GetStream()
+                networkStream.Read(bytesFrom, 0, CInt(clientSocket.ReceiveBufferSize))
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom)
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
+                dataFromClient = ParceMessage(dataFromClient)
+                If dataFromClient.ToLower = "exit" Then
+                    clientsList.Remove(clName)
+                    Exit While
+                End If
 
-    '            Private Sub doChat()
-    '                Dim infiniteCounter As Integer
-    '                Dim requestCount As Integer
-    '                Dim bytesFrom(10024) As Byte
-    '                Dim dataFromClient As String
-    '                Dim sendBytes As [Byte]()
-    '                Dim serverResponse As String
-    '                Dim rCount As String
-    '                requestCount = 0
-    '                For infiniteCounter = 1 To 2
-    '                    infiniteCounter = 1
-    '                    Try
-    '                        requestCount = requestCount + 1
-    '                        Dim networkStream As NetworkStream = _
-    '                                clientSocket.GetStream()
-    '                        networkStream.Read(bytesFrom, 0, CInt(clientSocket.ReceiveBufferSize))
-    '                        dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom)
-    '                        dataFromClient = _
-    '                            dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
-    '                        dataFromClient = ParceMessage(dataFromClient)
-    '                        If dataFromClient.ToLower = "exit" Then
-    '                            clientsList.Remove(clNo)
-    '                            Exit For
-    '                        End If
-    '                        msg("From client - " + clNo + " : " + dataFromClient)
-    '                        rCount = Convert.ToString(requestCount)
-
-    '                        broadcast(dataFromClient, clNo, True)
-    '                    Catch ex As Exception
-    '                        MsgBox(ex.ToString)
-    '                    End Try
-    '                Next
-    '            End Sub
-
-    '            Private Function ParceMessage(ByVal message As String) As String
-    '                Dim response As String = message
-    '                If message.ToLower = "test" Then
-    '                    response = Now.ToShortDateString
-    '                End If
-    '                Return response
-    '            End Function
-
-    '        End Class
-    '    End Module
-
+                broadcast(dataFromClient, clName, True)
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End While
+    End Sub
 End Class
