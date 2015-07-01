51fa4c700267f3af5f503d8064505e44021401d1
Jacob Christ <jacob@pontech.com>
2015-07-01 09:53:20 -0700

    Client: Turned client into a class

---
+++ ClientServer/ClientServer/ClientClass.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Net.Sockets
+
+Public Class ClientClass
+    Event MessageRecieved(ByVal Message As String)
+
+    Dim clientSocket As New System.Net.Sockets.TcpClient()
+    Dim serverStream As NetworkStream
+    Dim readData As String
+
+    Dim ctThread As Threading.Thread
+    Dim die As Boolean = False
+
+    Public Sub Send(ByVal Message)
+        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(Message + "$")
+        serverStream.Write(outStream, 0, outStream.Length)
+        serverStream.Flush()
+    End Sub
+
+    Private Sub msg()
+        RaiseEvent MessageRecieved(readData)
+    End Sub
+
+    Public Sub connect(ByVal ChatName As String)
+        readData = "Connected to Chat Server ..."
+        msg()
+
+        clientSocket.Connect("127.0.0.1", 8888)
+        serverStream = clientSocket.GetStream()
+
+        ctThread = New Threading.Thread(AddressOf getMessage)
+        ctThread.IsBackground = True
+        ctThread.Start()
+
+        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(ChatName + "$")
+        serverStream.Write(outStream, 0, outStream.Length)
+        serverStream.Flush()
+    End Sub
+
+    Private Sub getMessage()
+        Try
+            Dim inStream(10024) As Byte
+            Dim buffSize As Integer
+            Dim numberOfBytesRead As Integer
+            While (Not die)
+                serverStream = clientSocket.GetStream()
+                buffSize = clientSocket.ReceiveBufferSize
+                If serverStream.DataAvailable() Then
+                    numberOfBytesRead = serverStream.Read(inStream, 0, buffSize)
+                    Dim returndata As String = System.Text.Encoding.ASCII.GetString(inStream, 0, numberOfBytesRead)
+                    readData = "" + returndata
+                    msg()
+                End If
+            End While
+        Catch ex As Exception
+            Console.WriteLine("getMessage exception: " + ex.Message)
+        End Try
+    End Sub
+
+    Public Sub Close()
+        'die = True
+        ctThread.Abort()
+        While ctThread.IsAlive()
+            Application.DoEvents()
+        End While
+
+        clientSocket.Close()
+    End Sub
+
+End Class
--- ClientServer/ClientServer/client.vb
+++ ClientServer/ClientServer/client.vb
@@ -2,17 +2,11 @@ Imports System.Net.Sockets
 Imports System.Text
 
 Public Class client
-    Dim clientSocket As New System.Net.Sockets.TcpClient()
-    Dim serverStream As NetworkStream
+    Dim WithEvents client_instance As New ClientClass()
     Dim readData As String
 
-    Dim ctThread As Threading.Thread
-    Dim die As Boolean = False
-
     Private Sub SendMessageButton_Click(sender As Object, e As EventArgs) Handles SendMessageButton.Click
-        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(Command.Text + "$")
-        serverStream.Write(outStream, 0, outStream.Length)
-        serverStream.Flush()
+        client_instance.Send(Command.Text)
     End Sub
 
     Private Sub Command_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Command.KeyPress
@@ -20,6 +14,11 @@ Public Class client
             SendMessageButton_Click(sender, Nothing)
             e.Handled = True
         End If
+    End Sub
+
+    Private Sub MessageRecieved(ByVal Message) Handles client_instance.MessageRecieved
+        readData = Message
+        msg()
     End Sub
 
     Private Sub msg()
@@ -31,50 +30,11 @@ Public Class client
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        readData = "Connected to Chat Server ..."
-        msg()
-        clientSocket.Connect("127.0.0.1", 8888)
-        'Label1.Text = "Client Socket Program - Server Connected ..."
-        serverStream = clientSocket.GetStream()
-
-        ctThread = New Threading.Thread(AddressOf getMessage)
-        ctThread.IsBackground = True
-        ctThread.Start()
-
-        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(ChatName.Text + "$")
-        serverStream.Write(outStream, 0, outStream.Length)
-        serverStream.Flush()
-
-    End Sub
-
-    Private Sub getMessage()
-        Try
-            Dim inStream(10024) As Byte
-            Dim buffSize As Integer
-            Dim numberOfBytesRead As Integer
-            While (Not die)
-                serverStream = clientSocket.GetStream()
-                buffSize = clientSocket.ReceiveBufferSize
-                If serverStream.DataAvailable() Then
-                    numberOfBytesRead = serverStream.Read(inStream, 0, buffSize)
-                    Dim returndata As String = System.Text.Encoding.ASCII.GetString(inStream, 0, numberOfBytesRead)
-                    readData = "" + returndata
-                    msg()
-                End If
-            End While
-        Catch ex As Exception
-            Console.WriteLine("getMessage exception: " + ex.Message)
-        End Try
+        client_instance.connect(ChatName.Text)
     End Sub
 
     Private Sub client_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-        'die = True
-        ctThread.Abort()
-        While ctThread.IsAlive()
-            Application.DoEvents()
-        End While
-
-        clientSocket.Close()
+        client_instance.Close()
     End Sub
 
 End Class
